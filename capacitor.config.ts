abdcd37fdbadbd4e11f59fd0bb3068f02f10f681
osroot <osamaessayed.el.4468@azhar.edu.eg>
2024-02-02 16:41:37 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'os-genshin-community',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
